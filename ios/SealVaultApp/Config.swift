682ec3e41282dc70dcb86e127a61b194ea68ce88
Agost Biro <agostbiro@gmail.com>
2022-10-21 17:23:53 +0200

    Set discover page as default home page

--- ios/SealVaultApp/Config.swift
+++ ios/SealVaultApp/Config.swift
@@ -7,7 +7,7 @@ import Foundation
 struct Config {
     static let dbFileDir = "database"
     static let dbFileName = "sealvault-db.sqlite3"
-    static let defaultHomePage = "https://search.brave.com"
+    static let defaultHomePage = "https://sealvault.org/discover/"
     static let searchProvider = URL(string: "https://search.brave.com/search")!
     static let searchQueryParamName = "q"
 }
