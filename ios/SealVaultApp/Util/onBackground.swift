4284bc9f5697783b9c60327f4e1a65e5b34ded15
Agost Biro <agostbiro@gmail.com>
2022-12-27 17:11:00 +0100

    Add onBackground and onInactive
    More reliable than the SwiftUI native scenePhase change observer
    which can cause surprises with multiple scenes.

---
+++ ios/SealVaultApp/Util/onBackground.swift
@@ -0,0 +1,24 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import SwiftUI
+
+// Based on https://stackoverflow.com/a/72177271
+extension View {
+    #if os(iOS)
+    func onBackground(_ f: @escaping () -> Void) -> some View {
+        self.onReceive(
+            NotificationCenter.default.publisher(for: UIApplication.didEnterBackgroundNotification),
+            perform: { _ in f() }
+        )
+    }
+    #else
+    func onBackground(_ f: @escaping () -> Void) -> some View {
+        self.onReceive(
+            NotificationCenter.default.publisher(for: NSApplication.didEnterBackgroundNotification),
+            perform: { _ in f() }
+        )
+    }
+    #endif
+}
