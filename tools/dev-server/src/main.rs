a0365e545ef02184f41768936c1e62c9f7b2d7d3
Agost Biro <agostbiro@gmail.com>
2023-01-17 14:08:42 +0100

    Fix linter

--- tools/dev-server/src/main.rs
+++ tools/dev-server/src/main.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use std::{fs::File, io::Read, path::PathBuf, sync::Arc};
+use std::sync::Arc;
 
 use axum::{
     body::{boxed, BoxBody},
