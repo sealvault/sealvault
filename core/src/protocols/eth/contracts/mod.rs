202b5333a036aa2fa76eaf734297b4130a9b69ba
Agost Biro <agostbiro@gmail.com>
2023-04-04 19:57:41 +0200

    Add more ERC20 methods to ABI

--- core/src/protocols/eth/contracts/mod.rs
+++ core/src/protocols/eth/contracts/mod.rs
@@ -6,11 +6,16 @@ use ethers::contract::abigen;
 abigen!(
     ERC20Contract,
     r#"[
+        function totalSupply() external view returns (uint256)
         function balanceOf(address account) external view returns (uint256)
         function decimals() external view returns (uint8)
         function symbol() external view returns (string memory)
-        function transfer(address to, uint256 amount) external returns (bool)
+        function transfer(address recipient, uint256 amount) external returns (bool)
+        function allowance(address owner, address spender) external view returns (uint256)
+        function approve(address spender, uint256 amount) external returns (bool)
+        function transferFrom( address sender, address recipient, uint256 amount) external returns (bool)
         event Transfer(address indexed from, address indexed to, uint256 value)
+        event Approval(address indexed owner, address indexed spender, uint256 value)
     ]"#,
     event_derives(serde::Deserialize, serde::Serialize)
 );
